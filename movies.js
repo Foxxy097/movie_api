473021ee16a0bd6aae4d19d286b9d015f3c63b67
Foxxy097 <95033090+Foxxy097@users.noreply.github.com>
2022-01-03 13:37:09 +0100

    Json with top 10 movies

---
+++ movies.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+let top10Movies = [
+  {title: 'Encanto'},
+  {title: 'Spider-Man: No Way Home '},
+  {title: 'Shang-Chi and the Legend of the Ten Rings'},
+  {title: 'Eternals'},
+  {title: 'Last Christmas'},
+  {title: 'Doctor Strange'},
+  {title: 'Doctor Strange 2'},
+  {title: 'Grinch'},
+  {title: 'The Witches'},
+  {title: 'Jumanji'}
+];
