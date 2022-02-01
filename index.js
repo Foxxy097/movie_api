19e196b8e71faeb1c339d4acbe3a499286d5ac25
Foxxy097 <95033090+Foxxy097@users.noreply.github.com>
2022-02-01 13:41:02 +0100

    update

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.get('/mymovies/director/:name', passport.authenticate('jwt', { session: fals
 app.post(
   '/users/',
   [
-    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username is required').isLength({min: 2}),
     check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
     check('Password', 'Password is required').not().isEmpty(),
     check('Email', 'Email does not appear to be valid').isEmail()
